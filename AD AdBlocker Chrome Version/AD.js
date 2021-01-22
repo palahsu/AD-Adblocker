5eee38c2172f927fc6bb77e65de53c72b4281cf0
Palash Ahmed <49250151+palahsu@users.noreply.github.com>
2021-01-22 22:17:07 +0600

    Add files via upload

---
+++ AD AdBlocker Chrome Version/AD.js
@@ -0,0 +1,22 @@
+const defaultFilters = [
+	"*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.zedo.com/*",
+        "*://*.oppo.com/*",
+        "*://*.datacamp.com/*",
+                
+]
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return { cancel: true }},
+    { urls: defaultFilters },
+    ["blocking"]
+)
+
